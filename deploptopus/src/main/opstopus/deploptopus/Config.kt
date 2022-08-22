30747b48deb962028887ec41380150b6d1618030
Adam Cooper <adam@adamcooper.sh>
2022-08-22 01:26:17 -0500

    Add config parser

---
+++ deploptopus/src/main/opstopus/deploptopus/Config.kt
@@ -0,0 +1,70 @@
+package opstopus.deploptopus
+
+import io.ktor.util.logging.KtorSimpleLogger
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.decodeFromString
+import kotlinx.serialization.json.Json
+import opstopus.deploptopus.github.events.EventType
+import opstopus.deploptopus.system.FileIO
+import opstopus.deploptopus.system.FileIOException
+import platform.posix.F_OK
+import platform.posix.access
+import platform.posix.exit
+
+/**
+ * Represents a single trigger
+ */
+@Serializable
+data class Trigger(
+    val on: EventType,
+    val command: String,
+    val user: String,
+    val host: String,
+    val port: UInt,
+    val key: String
+)
+
+/**
+ * Represents the configuration of this deploptopus.
+ */
+@Serializable
+data class Config(val triggers: List<Trigger>) {
+    companion object {
+        private val log = KtorSimpleLogger("Config")
+
+        /**
+         * Loads the configuration file into its data representation
+         */
+        fun loadConfiguration(args: Array<String>): Config {
+            // Try to find the file path via the -f argument
+            val configFilePosition = args.indexOf("-f") + 1
+            val configFilePath = if (configFilePosition == 0) {
+                "/etc/deploptopus/config.json"
+            } else {
+                args.getOrElse(configFilePosition) {
+                    log.error("Passed '-f', but no config file location provided.")
+                    exit(1)
+                    ""
+                }
+            }
+
+            // If config file doesn't exist, exit because we don't know how to handle requests
+            if (access(configFilePath, F_OK) != 0) {
+                log.error("Config file at $configFilePath doesn't exist.")
+                exit(1)
+            }
+
+            // Load the contents of the config into a String
+            val configFile: FileIO
+            try {
+                configFile = FileIO(configFilePath)
+            } catch (e: FileIOException) {
+                log.error("Could not read config file at $configFilePath")
+                exit(1)
+                // Make the compiler happy
+                throw e
+            }
+            return Json.decodeFromString(configFile.read())
+        }
+    }
+}
--- deploptopus/src/main/opstopus/deploptopus/github/events/EventType.kt
+++ deploptopus/src/main/opstopus/deploptopus/github/events/EventType.kt
@@ -1,11 +1,34 @@
 package opstopus.deploptopus.github.events
 
+import kotlinx.serialization.KSerializer
+import kotlinx.serialization.Serializable
+import kotlinx.serialization.descriptors.PrimitiveKind
+import kotlinx.serialization.descriptors.PrimitiveSerialDescriptor
+import kotlinx.serialization.descriptors.SerialDescriptor
+import kotlinx.serialization.encoding.Decoder
+import kotlinx.serialization.encoding.Encoder
 import opstopus.deploptopus.NotFound
 import kotlin.reflect.KClass
+
+internal object EventTypeSerializer : KSerializer<EventType> {
+    override val descriptor: SerialDescriptor = PrimitiveSerialDescriptor(
+        "EventType",
+        PrimitiveKind.STRING
+    )
+
+    override fun serialize(encoder: Encoder, value: EventType) {
+        encoder.encodeString(value.eventName)
+    }
+
+    override fun deserialize(decoder: Decoder): EventType {
+        return EventType[decoder.decodeString()]
+    }
+}
 
 /**
  * Represents a mapping of event names to their expected payloads
  */
+@Serializable(with = EventTypeSerializer::class)
 enum class EventType(val eventName: String, val payloadType: KClass<out EventPayload>) {
     PACKAGE("package", PackageEventPayload::class);
 
