800665b40a062efc80d043ce8e7421bf932412d0
Adam Cooper <adam@adamcooper.sh>
2022-09-18 14:15:25 -0500

    Correctly configure StatusTest

--- src/test/opstopus/deploptopus/test/StatusTest.kt
+++ src/test/opstopus/deploptopus/test/StatusTest.kt
@@ -1,18 +1,31 @@
 package opstopus.deploptopus.test
 
+import io.ktor.client.request.accept
 import io.ktor.client.request.get
 import io.ktor.client.statement.bodyAsText
+import io.ktor.http.ContentType
+import io.ktor.http.isSuccess
 import io.ktor.server.testing.testApplication
 import opstopus.deploptopus.Status
+import opstopus.deploptopus.serializersModule
+import opstopus.deploptopus.statusModule
 import kotlin.test.Test
 import kotlin.test.assertEquals
+import kotlin.test.assertTrue
 
 class StatusTest {
     @Test
     fun testStatus() = testApplication {
-        val response = client.get("/")
+        application {
+            this.serializersModule()
+            this.statusModule()
+        }
+        val response = this.client.get("/") {
+            accept(ContentType.Application.Json)
+        }
+        assertTrue(response.status.isSuccess(), message = response.status.toString())
         assertEquals(
-            """{version:"deploptopus version ${Status.version}"}""",
+            """{"version":"deploptopus version ${Status.version}"}""",
             response.bodyAsText()
         )
     }
