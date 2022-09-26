4eb917fa2a444acccc9652b09d80764bde2beac5
Adam Cooper <adam@adamcooper.sh>
2022-09-25 23:13:12 -0500

    Simplify repository payload

--- src/main/opstopus/deploptopus/github/events/Payloads.kt
+++ src/main/opstopus/deploptopus/github/events/Payloads.kt
@@ -37,106 +37,10 @@ data class SenderPayload(
 )
 
 @Serializable
-data class LicensePayload(
-    val key: String,
-    val name: String,
-    @SerialName("spdx_id") val spdxID: String,
-    val url: SerializableURL,
-    @SerialName("node_id") val nodeID: String
-)
+data class RepositoryPayload(val name: String, @SerialName("full_name") val fullName: String)
 
 @Serializable
-data class RepositoryPayload(
-    val id: UInt,
-    @SerialName("node_id") val nodeID: String,
-    val name: String,
-    @SerialName("full_name") val fullName: String,
-    @SerialName("private") val isPrivate: Boolean,
-    val owner: SenderPayload,
-    val organization: OrganizationPayload?,
-    @SerialName("html_url") val htmlURL: SerializableURL,
-    val description: String?,
-    val fork: Boolean,
-    val url: SerializableURL,
-    @SerialName("forks_url") val forksURL: SerializableURL,
-    @SerialName("keys_url") val keysURL: SerializableURL,
-    @SerialName("collaborators_url") val collaboratorsURL: SerializableURL,
-    @SerialName("teams_url") val teamsURL: SerializableURL,
-    @SerialName("hooks_url") val hooksURL: SerializableURL,
-    @SerialName("issue_events_url") val issueEventsURL: SerializableURL,
-    @SerialName("events_url") val eventsURL: SerializableURL,
-    @SerialName("assignees_url") val assigneesURL: SerializableURL,
-    @SerialName("branches_url") val branchesURL: SerializableURL,
-    @SerialName("tags_url") val tagsURL: SerializableURL,
-    @SerialName("blobs_url") val blobsURL: SerializableURL,
-    @SerialName("git_tags_url") val gitTagsURL: SerializableURL,
-    @SerialName("git_refs_url") val gitRefsURL: SerializableURL,
-    @SerialName("trees_url") val treesURL: SerializableURL,
-    @SerialName("statuses_url") val statusesURL: SerializableURL,
-    @SerialName("languages_url") val languagesURL: SerializableURL,
-    @SerialName("stargazers_url") val stargazersURL: SerializableURL,
-    @SerialName("contributors_url") val contributorsURL: SerializableURL,
-    @SerialName("subscribers_url") val subscribersURL: SerializableURL,
-    @SerialName("subscription_url") val subscriptionURL: SerializableURL,
-    @SerialName("commits_url") val commitsURL: SerializableURL,
-    @SerialName("git_commits_url") val gitCommitsURL: SerializableURL,
-    @SerialName("comments_url") val commentsURL: SerializableURL,
-    @SerialName("issue_comment_url") val issueCommentsURL: SerializableURL,
-    @SerialName("contents_url") val contentsURL: SerializableURL,
-    @SerialName("compare_url") val compareURL: SerializableURL,
-    @SerialName("merges_url") val mergesURL: SerializableURL,
-    @SerialName("archive_url") val archiveURL: SerializableURL,
-    @SerialName("downloads_url") val downloadsURL: SerializableURL,
-    @SerialName("issues_url") val issuesURL: SerializableURL,
-    @SerialName("pulls_url") val pullsURL: SerializableURL,
-    @SerialName("milestones_url") val milestonesURL: SerializableURL,
-    @SerialName("notifications_url") val notificationsURL: SerializableURL,
-    @SerialName("labels_url") val labelsURL: SerializableURL,
-    @SerialName("releases_url") val releasesURL: SerializableURL,
-    @SerialName("deployments_url") val deploymentsURL: SerializableURL,
-    @SerialName("created_at") val createdAt: Instant,
-    @SerialName("updated_at") val updatedAt: Instant,
-    @SerialName("pushed_at") val pushedAt: Instant,
-    @SerialName("git_url") val gitURL: SerializableURL,
-    @SerialName("ssh_url") val sshURL: String,
-    @SerialName("clone_url") val cloneURL: SerializableURL,
-    @SerialName("svn_url") val svnURL: SerializableURL,
-    val homepage: SerializableURL?,
-    val size: UInt,
-    @SerialName("stargazers_count") val stargazersCount: UInt,
-    @SerialName("watchers_count") val watchersCount: UInt,
-    val language: String,
-    @SerialName("has_issues") val hasIssues: Boolean,
-    @SerialName("has_projects") val hasProjects: Boolean,
-    @SerialName("has_downloads") val hasDownloads: Boolean,
-    @SerialName("has_wiki") val hasWiki: Boolean,
-    @SerialName("has_pages") val hasPages: Boolean,
-    @SerialName("forks_count") val forksCount: UInt,
-    @SerialName("mirror_url") val mirrorURL: SerializableURL?,
-    val archived: Boolean,
-    val disabled: Boolean,
-    @SerialName("open_issues_count") val openIssuesCount: UInt,
-    val license: LicensePayload?,
-    @SerialName("allow_forking") val allowForking: Boolean,
-    @SerialName("is_template") val isTemplate: Boolean,
-    val topics: List<String>,
-    val visibility: String,
-    @SerialName("web_commit_signoff_required") val webCommitSignoffRequired: Boolean,
-    val forks: UInt,
-    @SerialName("open_issues") val openIssues: UInt,
-    val watchers: UInt,
-    @SerialName("default_branch") val defaultBranch: String,
-    @SerialName("temp_clone_token") val tempCloneToken: String?
-)
-
-@Serializable
-data class PartialRepositoryPayload(
-    val id: UInt,
-    @SerialName("node_id") val nodeID: String,
-    val name: String,
-    @SerialName("full_name") val fullName: String,
-    @SerialName("private") val isPrivate: Boolean
-)
+data class PartialRepositoryPayload(val name: String, @SerialName("full_name") val fullName: String)
 
 @Serializable
 data class OrganizationPayload(
